09ce9418df7834d154c262274ee6d9f6a4352c3f
Syed Adam Ali <kingsilverali@gmail.com>
2026-02-03 19:16:08 +0530

    imrpoved

--- Frontend/src/assets/asset.js
+++ Frontend/src/assets/asset.js
@@ -1,7 +1,7 @@
 
 
 export const assets = {
-  dark_logo: "https://occupational-emerald-uoi6axh2m7.edgeone.app/dark-logo.png",
-  light_logo: "https://definite-amaranth-ozmnwqsfpi.edgeone.app/light-logo.png",
-  hero_image: "https://underground-aqua-mjp3css3b4.edgeone.app/hero-image.png",
+  dark_logo: "https://img.sanishtech.com/u/803e6338560ddff3d4c5508140486c3c.png",
+  light_logo: "https://img.sanishtech.com/u/3902dc1c8cb02307da28e9fa678167e7.png",
+  hero_image: "https://img.sanishtech.com/u/ec0542600c2f1d9363919a6fccf183d4.png",
 };
