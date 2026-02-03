4eac779e7f270886fdf288af90e36c205e34f4ae
Syed Adam Ali <kingsilverali@gmail.com>
2026-02-03 15:39:57 +0530

    new improvments

--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -1,9 +1,8 @@
-import light_logo from "./light_logo.png";
-import upload_area from "./upload_area.jpg";
+
 
 const assets = {
-  light_logo,
-  upload_area,
+  light_logo: "https://img.sanishtech.com/u/418af7790569a90e7716bd3eea182d5f.png",
+  upload_area: "https://img.sanishtech.com/u/cff1f6cc799ce98405869223cf0a9c00.jpg",
 };
 
 export default assets;
