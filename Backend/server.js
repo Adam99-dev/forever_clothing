c335877a5b688e253b6d2ae79a34e50c921c328d
Syed Adam Ali <kingsilverali@gmail.com>
2026-02-03 19:31:39 +0530

    Final Commit

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://forever-clothing-frontend-psi.vercel.app",
+    origin: ["https://forever-clothing-frontend-psi.vercel.app", "https://forever-clothing-admin-three.vercel.app"],
     credentials: true,
   }),
 );
