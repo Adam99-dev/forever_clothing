0b7bd2f3bc37be7cb309abaf5f59ad82b99ff33a
Syed Adam Ali <kingsilverali@gmail.com>
2026-02-03 15:43:14 +0530

    Backend improved

--- Backend/server.js
+++ Backend/server.js
@@ -23,6 +23,10 @@ app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
+app.get("/", (req, res) => {
+  res.send("API IS WORKING!");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
