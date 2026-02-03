2836cc95fba75f9be19237a5442e4e8c86b57273
Syed Adam Ali <kingsilverali@gmail.com>
2026-02-03 19:18:18 +0530

    imrpoved

--- Frontend/src/assets/asset.js
+++ Frontend/src/assets/asset.js
@@ -1,7 +1,6 @@
 
 
 export const assets = {
-  dark_logo: "https://img.sanishtech.com/u/803e6338560ddff3d4c5508140486c3c.png",
-  light_logo: "https://img.sanishtech.com/u/3902dc1c8cb02307da28e9fa678167e7.png",
+  light_logo: "https://img.sanishtech.com/u/803e6338560ddff3d4c5508140486c3c.png",
   hero_image: "https://img.sanishtech.com/u/ec0542600c2f1d9363919a6fccf183d4.png",
 };
