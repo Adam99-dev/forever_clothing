f4f7d64b6f0bb3e0bd1c313c0033c49dc3916c9c
Syed Adam Ali <kingsilverali@gmail.com>
2026-02-06 21:54:48 +0530

    k

--- Frontend/src/assets/asset.js
+++
@@ -1,6 +0,0 @@
-
-
-export const assets = {
-  light_logo: "https://img.sanishtech.com/u/803e6338560ddff3d4c5508140486c3c.png",
-  hero_image: "https://img.sanishtech.com/u/ec0542600c2f1d9363919a6fccf183d4.png",
-};
