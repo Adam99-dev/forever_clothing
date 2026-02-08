074517369c45e400ea3a3869c35dc84e2742b7d0
Syed Adam Ali <kingsilverali@gmail.com>
2026-02-08 14:58:36 +0530

    .

--- Desktop/main.js
+++ Desktop/main.js
@@ -5,11 +5,14 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    autoHideMenuBar: true, // removes top menu/header
-    icon: path.join(__dirname, "./assets/icon.ico"), // app icon
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, "./assets/icon.ico"),
   });
 
   win.loadFile(path.join(__dirname, "../Admin/dist/index.html"));
+
+  // ✅ Open DevTools
+  win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
