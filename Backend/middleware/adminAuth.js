0047cce67c8d686fea06e8dfa2c5864f506a3b1d
Syed Adam Ali <kingsilverali@gmail.com>
2026-02-08 22:31:26 +0530

    Order Page Error..

--- Backend/middleware/adminAuth.js
+++ Backend/middleware/adminAuth.js
@@ -1,24 +1,25 @@
 import jwt from "jsonwebtoken";
 
-const adminAuth = async (req, res, next) => {
+const adminAuth = (req, res, next) => {
   try {
-    const { token } = req.headers;
+    const token =
+      req.headers.token || req.headers.authorization?.split(" ")[1];
+
     if (!token) {
-      res.json({ success: "false", message: "Not Authorized, Login Again" });
-    } else {
-      const token_decoded = jwt.verify(token, process.env.JWT_SECRET);
-      if (
-        token_decoded !==
-        process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD
-      ) {
-        res.json({ success: "false", message: "Not Authorized, Login Again" });
-      }
-      next();
+      return res.json({ success: false, message: "Not Authorized" });
     }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.email !== process.env.ADMIN_EMAIL) {
+      return res.json({ success: false, message: "Not Authorized" });
+    }
+
+    next();
   } catch (error) {
-    console.log(error);
-    res.json({ success: "false", message: error.message });
+    console.log(error.message);
+    return res.json({ success: false, message: error.message });
   }
 };
 
-export default adminAuth
+export default adminAuth;
